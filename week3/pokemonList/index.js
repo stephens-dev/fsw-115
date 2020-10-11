aaabc5f9c8e80ac7480cc62c5866d7a747484c4e
stephensWebDev <53384838+stephensWebDev@users.noreply.github.com>
2020-10-11 08:22:53 -0500

    week3

--- week3/pokemonList/index.js
+++ week3/pokemonList/index.js
@@ -2,12 +2,13 @@ const getPoke = () => {
     let xhr = new XMLHttpRequest()
     console.log(xhr)
     xhr.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200){
-            let data = xhr.response
-            renderPoke(data)
+        if(xhr.readyState === 4 && xhr.status === 200){
+            const JSONdata = xhr.responseText
+            let data = JSON.parse(JSONdata)
+            renderPoke(data.objects[0])
         }
     }
-    xhr.open('GET', 'https://api.vschool.io/pokemon')
+    xhr.open('GET', 'https://api.vschool.io/pokemon',true)
     xhr.send()
 }
 
@@ -15,10 +16,10 @@ let list = document.getElementById("root")
 
  function renderPoke(data) {
      console.log(data)
-    for (var i = 0; i < data.length; i++) {
-        var newName = document.createElement("li")
-        newName.textContent = data[i].name
-        list.append(newName)
+    for (var i = 0; i < data.pokemon.length; i++) {
+        var newName = document.createElement("h1")
+        newName.textContent = data.pokemon[i].name
+        document.body.append(newName)
     }
 }
 
